1d9e9be012faf587ca784abad332f3fe07928d28
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-12-08 14:30:43 -0500

    feat: ✨ solve day 1 part 1

--- src/bin/01a.rs
+++ src/bin/01a.rs
@@ -1,3 +1,30 @@
+use advent_of_code_2024::input_lines;
+
 fn main() {
-    todo!();
+    let input = input_lines().map(|line| {
+        let mut row = line
+            .split_whitespace()
+            .map(|string| string.parse::<u32>().expect("msg"));
+        let first = row.next().expect("Input row should not be empty");
+        let second = row.next().expect("Input row should have 2 values");
+        (first, second)
+    });
+
+    let mut first_list = Vec::new();
+    let mut second_list = Vec::new();
+    for (first, second) in input {
+        first_list.push(first);
+        second_list.push(second);
+    }
+
+    first_list.sort_unstable();
+    second_list.sort_unstable();
+
+    let answer: u32 = first_list
+        .into_iter()
+        .zip(second_list)
+        .map(|(first, second)| first.abs_diff(second))
+        .sum();
+
+    println!("{answer}");
 }
