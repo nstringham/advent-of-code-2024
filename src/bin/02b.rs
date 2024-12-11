d254b3dfa0962bcd9e078e6bd0fbaa29220a9e66
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-12-11 13:32:45 -0500

    feat: ✨ solve day 2 part 2

--- src/bin/02b.rs
+++ src/bin/02b.rs
@@ -1,3 +1,33 @@
+use advent_of_code_2024::parsed_input_lines;
+
 fn main() {
-    todo!();
+    let reports = parsed_input_lines::<u8>();
+
+    fn is_safe(report: &[u8]) -> bool {
+        let rising = report.first() < report.last();
+        report
+            .iter()
+            .zip(report.iter().skip(1))
+            .all(|(&a, &b)| (a < b) == rising && (1..=3).contains(&a.abs_diff(b)))
+    }
+
+    let safe_reports = reports.filter(|report| {
+        if is_safe(report) {
+            return true;
+        }
+
+        for i in 0..report.len() {
+            let mut report = report.clone();
+            report.remove(i);
+            if is_safe(&report) {
+                return true;
+            }
+        }
+
+        false
+    });
+
+    let answer = safe_reports.count();
+
+    println!("{answer}");
 }
