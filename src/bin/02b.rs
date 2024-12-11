4db274ad7abfd9b499c247ce39f7ef6e80573ced
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-12-11 18:07:41 -0500

    refactor: ⏪ make day 2 part 2 simpler
    This reverts commit 2c3ac365ee7dd1e49496cf8184da0fcc5ff10281.

--- src/bin/02b.rs
+++ src/bin/02b.rs
@@ -1,67 +1,30 @@
 use advent_of_code_2024::parsed_input_lines;
 
-fn valid_decreasing_step(step: i8) -> bool {
-    (-3..=-1).contains(&step)
-}
-
-fn valid_increasing_step(step: i8) -> bool {
-    (1..=3).contains(&step)
-}
-
 fn main() {
-    let reports = parsed_input_lines::<i8>();
+    let reports = parsed_input_lines::<u8>();
+
+    fn is_safe(report: &[u8]) -> bool {
+        let rising = report.first() < report.last();
+        report
+            .iter()
+            .zip(report.iter().skip(1))
+            .all(|(&a, &b)| (a < b) == rising && (1..=3).contains(&a.abs_diff(b)))
+    }
 
     let safe_reports = reports.filter(|report| {
-        let mut decreasing_skip = None;
-        let mut increasing_skip = None;
-        let mut could_be_decreasing = true;
-        let mut could_be_increasing = true;
+        if is_safe(report) {
+            return true;
+        }
 
-        let step_without = |i: usize| report[i + 1] - report[i - 1];
-
-        for i in 1..report.len() {
-            let step = report[i] - report[i - 1];
-
-            if could_be_decreasing && !valid_decreasing_step(step) && decreasing_skip != Some(i - 1)
-            {
-                if decreasing_skip.is_none()
-                    && (i == report.len() - 1 || valid_decreasing_step(step_without(i)))
-                {
-                    decreasing_skip = Some(i);
-                } else if decreasing_skip.is_none()
-                    && (i == 1 || valid_decreasing_step(step_without(i - 1)))
-                {
-                    decreasing_skip = Some(i - 1);
-                } else {
-                    if could_be_increasing {
-                        could_be_decreasing = false;
-                    } else {
-                        return false;
-                    }
-                }
-            }
-
-            if could_be_increasing && !valid_increasing_step(step) && increasing_skip != Some(i - 1)
-            {
-                if increasing_skip.is_none()
-                    && (i == report.len() - 1 || valid_increasing_step(step_without(i)))
-                {
-                    increasing_skip = Some(i);
-                } else if increasing_skip.is_none()
-                    && (i == 1 || valid_increasing_step(step_without(i - 1)))
-                {
-                    increasing_skip = Some(i - 1);
-                } else {
-                    if could_be_decreasing {
-                        could_be_increasing = false;
-                    } else {
-                        return false;
-                    }
-                }
+        for i in 0..report.len() {
+            let mut report = report.clone();
+            report.remove(i);
+            if is_safe(&report) {
+                return true;
             }
         }
 
-        true
+        false
     });
 
     let answer = safe_reports.count();
